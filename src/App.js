91c3231ce213143d552b5f595f5f06311163df48
Truong Duc Binh <bpm.binhtd@mbageas.life>
2021-05-18 17:31:23 +0700

    init webpack

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// import Recipes from "./Recipes";
+// import sword from "../images/swc-sword.png";
+// import swordSvg from "../images/sword.svg";
+
+import "styles/index.scss";
+
+const App = () => {
+  return (
+    <>
+      <section className="hero"></section>
+      <main>
+        <section>
+          <h1>Oh Herro, React.</h1>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default App;
